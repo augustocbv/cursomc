f26d4ef96ef69f521fe4f81793a54f63db8731bd
Augusto Bevilaqua <augustocbv@hotmail.com>
2022-01-21 17:58:11 -0300

    Teste 1a classe - Categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,35 @@
 package com.nelioalves.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcional!";
+	public List<Categoria> listar() {
+	//1.1 teste do rest
+	//public String listar() {
+		
+		// 2 retornando lista categorias
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Informatica");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
+		//1.2 teste do rest
+		//return "REST está funcional!";
 	}
 	
 }
